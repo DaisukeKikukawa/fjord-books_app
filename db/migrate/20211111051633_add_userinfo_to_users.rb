2cea23e4cd063e90bcf39790f2403dfb1157e4d5
Daisuke <a1485@icloud.com>
2021-11-11 16:04:18 +0900

    ユーザー情報の追加

---
+++ db/migrate/20211111051633_add_userinfo_to_users.rb
@@ -0,0 +1,8 @@
+class AddUserinfoToUsers < ActiveRecord::Migration[6.1]
+  def change
+    add_column :users, :username, :string
+    add_column :users, :postal_code, :string
+    add_column :users, :address, :text
+    add_column :users, :description, :text
+  end
+end
