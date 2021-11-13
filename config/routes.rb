060cea54a35890e681cf82437c25ecf549ff9d4e
Daisuke <a1485@icloud.com>
2021-11-13 21:55:00 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'books#index'
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations',
+                                    sessions: 'users/sessions' }
   resources :books
+  resources :users, only: [:index, :show]
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
