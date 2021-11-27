45fdaa19a0271a3d1b0dc6b00deb526bd86bbde9
Daisuke <a1485@icloud.com>
2021-11-27 22:12:07 +0900

    fix:カラム名の変更

--- db/schema.rb
+++ db/schema.rb
@@ -57,10 +57,10 @@ ActiveRecord::Schema.define(version: 2021_11_16_132859) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "name"
+    t.string "username"
     t.string "postal_code"
-    t.string "address"
-    t.text "self_introduction"
+    t.text "address"
+    t.text "description"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
