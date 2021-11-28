905fff06d5ac0ea702508fe2358050bdc3527cba
Daisuke <a1485@icloud.com>
2021-11-28 23:25:12 +0900

    アソシエーションの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,23 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+
+  ham_many :followers,
+            class_name: 'Follow',
+            foreign_key: 'follower_id',
+            dependent: :destroy,
+            inverse_of: :follower
+
+  ham_many :followings,
+            class_name: 'Follow',
+            foreign_key: 'following_id',
+            dependent: :destroy,
+            inverse_of: :following
+
+  has_many :following_users, through: :followers, source: :following
+
+  has_many :follower_users, through: :followings, source: :follower
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
