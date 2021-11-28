710e5a1a085a54b5077859e70c3c2986b63a64e8
Daisuke <a1485@icloud.com>
2021-11-28 18:19:47 +0900

    マイグレーションファイルにユニーク制約の追加

--- db/migrate/20211128085617_create_follows.rb
+++ db/migrate/20211128085617_create_follows.rb
@@ -6,5 +6,8 @@ class CreateFollows < ActiveRecord::Migration[6.1]
 
       t.timestamps
     end
+    add_index :follows, :following_id
+    add_index :follows, :follower_id
+    add_index :follows, [:following_id, :follower_id], unique: true
   end
 end
