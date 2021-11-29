934876c79a6b4fb1bbe5ed9d6f12dec05aadf269
Daisuke <a1485@icloud.com>
2021-11-29 22:14:39 +0900

    フォロー・フォロワー一覧画面の作成

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,6 +1,6 @@
 class Follow < ApplicationRecord
   belongs_to :follower, class_name: 'User'
   belongs_to :following, class_name: 'User'
-  validates :follower_id, presense: true
-  validates :following_id, presense: true
+  # validates :follower_id, presense: true
+  # validates :following_id, presense: true
 end
