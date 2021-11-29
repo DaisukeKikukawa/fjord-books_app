58ab716b328989e867dc80309ffc3373fc524ac7
Daisuke <a1485@icloud.com>
2021-11-29 12:10:34 +0900

    コントローラーにアクションの追加

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,2 +1,14 @@
 class FollowsController < ApplicationController
+
+  def create
+    user = User.find(params[:follow_id])
+    current_user.follow(params[:follow_id])
+    redirect_to user_path(user)
+  end
+
+  def destroy
+    Follow.find_by(follower_id: current_user.id, following_id: params[:id]).destroy
+    redirect_to user_path
+  end
+
 end
