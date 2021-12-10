d542139d30944b842ac4e8fcf13523b1727911e0
Daisuke <a1485@icloud.com>
2021-12-10 23:25:21 +0900

    不要アクションの削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,10 @@ class UsersController < ApplicationController
     @users = User.with_attached_avatar.order(:id).page(params[:page])
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   def followings
     @followings = @user.following_users
   end
