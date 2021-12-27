719d93f7912a2c5b55439bd76cc56aa39c378dba
Daisuke <a1485@icloud.com>
2021-12-27 18:33:33 +0900

    [fix]report_test.rbのモデルテストの修正

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -4,20 +4,17 @@ require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
   test '#editable?' do
-    me = User.create(email: 'me@example.com', password: 'password')
-    she = User.create(email: 'she@example.com', password: 'password')
-    report = Report.create(user_id: me.id, title: '初めまして', content: 'こんにちは')
+    owner = User.create(email: 'me@example.com', password: 'password')
+    other_user = User.create(email: 'she@example.com', password: 'password')
+    report = Report.create!(user_id: owner.id, title: '初めまして', content: 'こんにちは')
 
-    assert report
-    assert report.editable?(me)
-    assert_not report.editable?(she)
+    assert report.editable?(owner)
+    assert_not report.editable?(other_user)
   end
 
   test '#created_on' do
-    me = User.create(email: 'me@example.com', password: 'password')
-    report = Report.create(user_id: me.id, title: '初めまして', content: 'こんにちは')
+    owner = User.create(email: 'me@example.com', password: 'password')
+    report = Report.create!(user_id: owner.id, title: '初めまして', content: 'こんにちは')
 
-    assert report
-    assert_not_equal Date, report.created_at.class
   end
 end
